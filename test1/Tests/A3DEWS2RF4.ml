2fb4dbab2ebde07482ae1e843f9c78bee1c873b9
revanthpush <revdeveng@gmail.com>
2022-11-10 11:41:39 +0530

    This is first commit

---
+++ test1/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/test/Tests/test1.bwt#/"/>
+  <EmulationDataFile>ACED000574000974657374312E627774</EmulationDataFile>
+</emulation:BWTFile>
